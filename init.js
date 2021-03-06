330e5230a6dc59b0b5df16c1c469e5261eeebe6e
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-03-06 17:48:55 +0000

    bump to version 1.0.0

--- init.js
+++ init.js
@@ -3,3 +3,13 @@ import {start} from './main.js';
 const name = "wasm/rustynes";
 window.Module = {
   preRun: [],
+  postRun: [],
+  wasmBinaryFile: `${name}.wasm`,
+  noExitRuntime: true,
+  print: text => {
+    console.log(text);
+  },
+  printErr: text => {
+    console.error(text);
+  },
+  onRuntimeInitialized() 
