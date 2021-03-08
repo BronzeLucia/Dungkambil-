25b5266023b385cef2a2ce21093dde76afd3aaeb
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-03-08 14:42:41 +0000

    Improved code.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import Oscillator
+import Oscillator from './src/nes/browser/oscillator.js';
+import Noise from './src/nes/browser/noise.js';
+
+let buf = null
+
+const convertKeyCode = (keyCode) => {
+  switch (keyCode) {
+  case 88: return 0x01; // X  A
+  case 90: return 0x02; // Z 
