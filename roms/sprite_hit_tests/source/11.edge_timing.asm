e7428f868708bfa813d19b58e15bb1f182f2f16f
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-03-27 20:43:47 +0000

    Update jenkins.md

--- roms/sprite_hit_tests/source/11.edge_timing.asm
+++ roms/sprite_hit_tests/source/11.edge_timing.asm
@@ -1,4 +1,17 @@
 ; Tests sprite 0 hit timing for which pixel it first reports hit on
 ; when some pixels are under clip, or at or beyond right edge.
 
-      .
+      .include "prefix_sprite_hit.a"
+
+test_name:
+      .db   "SPRITE HIT EDGE TIMING",0
+      .code
+
+reset:
+      jsr   begin_sprite_hit_tests
+      
+      lda   #solid_tile
+      jsr   fill_nametable
+
+      lda   #120
+      sta   sprite_
