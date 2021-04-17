d9044d98acbc2794dcd99e085a21f0fd6dc98ca2
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-04-17 07:51:14 +0000

    increase font size

--- roms/sprite_ram.asm
+++ roms/sprite_ram.asm
@@ -54,3 +54,79 @@ reset:
       lda   $2004
       cmp   #$34
       jsr   error_if_eq
+      
+      lda   #5;) Third sprite bytes should be masked with $e3 on read 
+      sta   result
+      lda   #3
+      sta   $2003
+      lda   #$ff
+      sta   $2004
+      lda   #3
+      sta   $2003
+      lda   $2004
+      cmp   #$e3
+      jsr   error_if_eq
+      
+      lda   #6;) $4014 DMA copy doesn't work at all
+      sta   result
+      ldx   #0          ; set up data to copy from
+:     lda   test_data,x
+      sta   sprites,x
+      inx
+      cpx   #4
+      bne   -
+      lda   #0          ; dma copy
+      sta   $2003
+      lda   #$02
+      sta   $4014
+      ldx   #0          ; set up data to copy from
+:     stx   $2003
+      lda   $2004
+      cmp   test_data,x
+      jsr   error_if_ne
+      inx
+      cpx   #4
+      bne   -
+      
+      lda   #7;) $4014 DMA copy should start at value in $2003 and wrap
+      sta   result
+      ldx   #0          ; set up data to copy from
+:     lda   test_data,x
+      sta   sprites,x
+      inx
+      cpx   #4
+      bne   -
+      lda   #1          ; dma copy
+      sta   $2003
+      lda   #$02
+      sta   $4014
+      ldx   #1          ; set up data to copy from
+:     stx   $2003
+      lda   $2004
+      cmp   sprites - 1,x
+      jsr   error_if_ne
+      inx
+      cpx   #5
+      bne   -
+      
+      lda   #8;) $4014 DMA copy should leave value in $2003 intact
+      sta   result
+      lda   #1          ; dma copy
+      sta   $2003
+      lda   #$02
+      sta   $4014
+      lda   #$ff
+      sta   $2004
+      lda   #1
+      sta   $2003
+      lda   $2004
+      cmp   #$ff
+      jsr   error_if_ne
+      
+      lda   #1;) Tests passed
+      sta   result
+      jmp   report_final_result
+      
+test_data:
+      .db   $12,$82,$e3,$78
+      
---
+++ roms/vram_access.asm
@@ -0,0 +1,4 @@
+; Tests PPU VRAM read/write and internal read buffer operation
+
+      .include "prefix_ppu.a"
+
