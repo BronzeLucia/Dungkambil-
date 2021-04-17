57c7e451d487c0a5011602f662f01696fe9f932c
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-04-17 07:51:50 +0000

    initial working commit

--- roms/vram_access.asm
+++ roms/vram_access.asm
@@ -2,3 +2,18 @@
 
       .include "prefix_ppu.a"
 
+; Set VRAM addr to $2f00 + A
+; Preserved: A, X, Y
+set_vram_pos:
+      pha
+      lda   #$2f
+      sta   $2006
+      pla
+      sta   $2006
+      rts
+
+reset:
+      lda   #50
+      jsr   delay_msec
+      
+      jsr   wait_vbl
