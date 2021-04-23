9ac3f1fcbaca9682e23ddb625be0ceab19368a40
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-04-23 08:45:07 +0000

    remove pdb

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,11 @@ extern crate libc;
 mod nes;
 mod externs;
 
-use nes::Con
+use nes::Context;
+
+fn main() {}
+
+#[no_mangle]
+pub fn run(len: usize, ptr: *mut u8) {
+    let buf: &mut [u8] = unsafe { std::slice::from_raw_parts_mut(ptr, len + 1) };
+    let mut ct
