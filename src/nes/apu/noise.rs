65dac2f858fac6e0309333bfe422564e471f7f71
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-04-25 22:44:55 +0000

    updated Polish translation

--- src/nes/apu/noise.rs
+++ src/nes/apu/noise.rs
@@ -1,1 +1,8 @@
-use super::constants::{NOISE_TIMER_PERIO
+use super::constants::{NOISE_TIMER_PERIOD_TABLE, GROBAL_GAIN, CPU_CLOCK, COUNTER_TABLE};
+use nes::types::{Data, Addr};
+
+#[derive(Debug)]
+pub struct Noise {
+    envelope_generator_counter: usize,
+    envelope_rate: usize,
+    envelope
