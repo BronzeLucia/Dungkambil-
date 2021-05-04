797becf2bb0168efd00fc2d8b14316caee05be18
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-05-04 14:51:59 +0000

    Updated Dutch translation.

--- src/nes/browser/oscillator.js
+++ src/nes/browser/oscillator.js
@@ -1,3 +1,10 @@
 
 import pulse from './pulse.js';
 
+export default class Oscillator {
+
+  constructor(type) {
+    try {
+      const AudioContext = window.AudioContext || window.webkitAudioContext
+      this.context = new AudioContext();
+    } ca
