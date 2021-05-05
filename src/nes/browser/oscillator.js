6c1a83853719dc9fe63a56580db8654155bcf7aa
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-05-05 05:49:09 +0000

    Ignore .idea folder

--- src/nes/browser/oscillator.js
+++ src/nes/browser/oscillator.js
@@ -7,4 +7,10 @@ export default class Oscillator {
     try {
       const AudioContext = window.AudioContext || window.webkitAudioContext
       this.context = new AudioContext();
-    } ca
+    } catch (e) {
+      throw new Error('Web Audio isn\'t supported in this browser!');
+    }
+    this.type = type || 'square';
+    this.oscillator = this.createOscillator({ kind: this.type });
+
+    this.wa
