3a83bd456d0e2b8499c4dbbe098074cdd1f0ed7e
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-05-10 09:41:03 +0000

    clean up repo

--- src/nes/cpu/instructions.rs
+++ src/nes/cpu/instructions.rs
@@ -6,4 +6,11 @@ use super::super::helper::*;
 pub fn process_nmi<T: CpuRegisters, U: CpuBus>(registers: &mut T, bus: &mut U) {
     registers.set_break(false);
     push((registers.get_PC() >> 8) as u8, registers, bus);
-    push(registers.get_
+    push(registers.get_PC() as u8, registers, bus);
+    push_status(registers, bus);
+    registers.set_interrupt(true);
+    let next = bus.read_word(0xFFFA);
+    registers.set_PC(next);
+}
+
+pub fn lda<T: CpuRegisters, U: CpuBus>(operand
