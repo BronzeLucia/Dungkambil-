d056763ebf44fe80ee4284a43cafcd39a3486fac
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2021-05-10 08:17:04 +0000

    link test

--- src/nes/cpu/instructions.rs
+++ src/nes/cpu/instructions.rs
@@ -1,4 +1,9 @@
 use super::super::cpu_registers::CpuRegisters;
 use super::super::bus::cpu_bus::CpuBus;
 use super::super::types::{Data, Addr, Word};
-use sup
+use super::super::helper::*;
+
+pub fn process_nmi<T: CpuRegisters, U: CpuBus>(registers: &mut T, bus: &mut U) {
+    registers.set_break(false);
+    push((registers.get_PC() >> 8) as u8, registers, bus);
+    push(registers.get_
