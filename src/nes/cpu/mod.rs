a7b56c854b8d7edd265d582a688e7ea2868168b6
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-03-31 22:17:58 +0000

    Upload images

--- src/nes/cpu/mod.rs
+++ src/nes/cpu/mod.rs
@@ -7,4 +7,10 @@ use self::instructions::*;
 use self::opecode::*;
 use std::fmt::Debug;
 
-use supe
+use super::bus::cpu_bus::CpuBus;
+use super::cpu_registers::CpuRegisters;
+use super::types::Data;
+
+pub fn reset<T: CpuRegisters, U: CpuBus>(registers: &mut T, bus: &mut U) {
+    let pc = bus.read_word(0xFFFC);
+   
