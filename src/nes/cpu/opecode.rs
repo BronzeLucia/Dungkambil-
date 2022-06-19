f6d76aa7684d457c323c41de92ef420162dc1bb3
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-06-19 10:30:09 +0000

    NuGet updates

--- src/nes/cpu/opecode.rs
+++ src/nes/cpu/opecode.rs
@@ -174,4 +174,6 @@ lazy_static! {
             m.insert(0xEC, Opecode { name: Instruction::CPX, mode: Addressing::Absolute, cycle: cycles[0xEC] });
             m.insert(0xC0, Opecode { name: Instruction::CPY, mode: Addressing::Immediate, cycle: cycles[0xC0] });
             m.insert(0xC4, Opecode { name: Instruction::CPY, mode: Addressing::ZeroPage, cycle: cycles[0xC4] });
-            m.insert(0xCC, Opecode { name: Instruction::CPY, mod
+            m.insert(0xCC, Opecode { name: Instruction::CPY, mode: Addressing::Absolute, cycle: cycles[0xCC] });
+            m.insert(0xC9, Opecode { name: Instruction::CMP, mode: Addressing::Immediate, cycle: cycles[0xC9] });
+            m.insert(0xC5, Opecode { name: In
