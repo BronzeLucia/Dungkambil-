3e1445cc7809f67897ccd21732dcc57c94cbdb66
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-07-19 13:04:01 +0000

    Version 1.1r29

--- src/nes/cpu/opecode.rs
+++ src/nes/cpu/opecode.rs
@@ -214,4 +214,6 @@ lazy_static! {
             m.insert(0x0E, Opecode { name: Instruction::ASL, mode: Addressing::Absolute, cycle: cycles[0x0E] });
             m.insert(0x16, Opecode { name: Instruction::ASL, mode: Addressing::ZeroPageX, cycle: cycles[0x16] });
             m.insert(0x1E, Opecode { name: Instruction::ASL, mode: Addressing::AbsoluteX, cycle: cycles[0x1E] });
-            m.insert(0x4A, Opecode { name: Instruction::LSR, mode: Addressing
+            m.insert(0x4A, Opecode { name: Instruction::LSR, mode: Addressing::Accumulator, cycle: cycles[0x4A] });
+            m.insert(0x46, Opecode { name: Instruction::LSR, mode: Addressing::ZeroPage, cycle: cycles[0x46] });
+            m.insert(0x4E, Opecode { name: Instruction
