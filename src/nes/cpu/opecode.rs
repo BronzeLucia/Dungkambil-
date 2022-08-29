f15d9c46168d0f59069f98937c4aff3cbf71b330
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-08-29 18:51:48 +0000

    Fixed #32

--- src/nes/cpu/opecode.rs
+++ src/nes/cpu/opecode.rs
@@ -313,4 +313,6 @@ lazy_static! {
             m.insert(0x8F, Opecode { name: Instruction::SAX, mode: Addressing::Absolute, cycle: cycles[0x8F] });
             m.insert(0x83, Opecode { name: Instruction::SAX, mode: Addressing::PreIndexedIndirect, cycle: cycles[0x83] });
             m.insert(0xEB, Opecode { name: Instruction::SBC, mode: Addressing::Immediate, cycle: cycles[0xEB] });
-            m.insert(0xC7, Opecode { name: Instruction::DCP, mode: Addressing
+            m.insert(0xC7, Opecode { name: Instruction::DCP, mode: Addressing::ZeroPage, cycle: cycles[0xC7] });
+            m.insert(0xD7, Opecode { name: Instruction::DCP, mode: Addressing::ZeroPageX, cycle: cycles[0xD7] });
+            m.insert(0xCF, Opecode { name: Instruction
