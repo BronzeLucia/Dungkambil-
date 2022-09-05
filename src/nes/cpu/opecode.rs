2eaa58acd365353f441dd49750bd473b8efdfd2e
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-09-05 14:04:07 +0000

    Added pagination

--- src/nes/cpu/opecode.rs
+++ src/nes/cpu/opecode.rs
@@ -333,4 +333,5 @@ lazy_static! {
             m.insert(0x1F, Opecode { name: Instruction::SLO, mode: Addressing::AbsoluteX, cycle: cycles[0x1F] });
             m.insert(0x1B, Opecode { name: Instruction::SLO, mode: Addressing::AbsoluteY, cycle: cycles[0x1B] });
             m.insert(0x03, Opecode { name: Instruction::SLO, mode: Addressing::PreIndexedIndirect, cycle: cycles[0x03] });
-         
+            m.insert(0x13, Opecode { name: Instruction::SLO, mode: Addressing::PostIndexedIndirect, cycle: cycles[0x13] });
+            m.insert(0x27, Opecode { name: Instruction::RLA, mode: Add
