bdd7b09d0957bb88a51dd5157102449e89786a0c
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-10-02 22:05:57 +0000

    fnord-webcomponents update

--- src/nes/mmc/mod.rs
+++ src/nes/mmc/mod.rs
@@ -5,4 +5,13 @@ pub struct Mmc {
 }
 
 impl Mmc {
-    pub fn n
+    pub fn new(mapper: u8, bank: u8) -> Self {
+        Mmc { bank, mapper }
+    }
+
+    pub fn set_bank(&mut self, bank: u8) {
+        self.bank = bank;
+    }
+
+    pub fn get_bank(&self) -> u8 {
+        self.ban
