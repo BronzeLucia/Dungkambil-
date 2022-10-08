7208fba2405be0c50d8137aa7c04f40a6ea9531b
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-10-08 13:06:49 +0000

    updated to do

--- src/nes/parser/mod.rs
+++ src/nes/parser/mod.rs
@@ -2,4 +2,13 @@ use std::str;
 
 const NES_HEADER_SIZE: usize = 0x0010;
 const PROGRAM_ROM_SIZE: usize = 0x4000;
-c
+const CHARACTER_ROM_SIZE: usize = 0x2000;
+
+pub struct Cassette {
+    pub is_horizontal_mirror: bool,
+    pub character_ram: Vec<u8>,
+    pub program_rom: Vec<u8>,
+    pub mapper: u8,
+}
+
+pub fn parse(buf: &mut [u8]
