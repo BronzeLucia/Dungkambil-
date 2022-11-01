bbf3d2cace89319d33de0182130012faa41c0411
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-11-01 16:16:49 +0000

    rails_apps_composer: testing framework

--- src/nes/ppu/registers/ppu_scroll.rs
+++ src/nes/ppu/registers/ppu_scroll.rs
@@ -1,2 +1,19 @@
 
-use super::super::super::types
+use super::super::super::types::Data;
+
+#[derive(Debug)]
+enum Enable {
+    X,
+    Y,
+}
+
+#[derive(Debug)]
+pub struct PpuScroll {
+    x: Data,
+    y: Data,
+    enable: Enable,
+}
+
+impl PpuScroll {
+    pub fn new() -> Self {
+  
