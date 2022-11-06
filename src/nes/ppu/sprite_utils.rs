fb934f1b1e398151a3c3a0d3927eb766615fb058
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-11-06 10:36:26 +0000

    score

--- src/nes/ppu/sprite_utils.rs
+++ src/nes/ppu/sprite_utils.rs
@@ -3,3 +3,11 @@ use super::super::Ram;
 use super::super::Mmc;
 
 pub type Sprite = Vec<Vec<u8>>;
+
+pub type SpritePosition = (u8, u8);
+
+#[derive(Debug)]
+pub struct SpriteConfig {
+    pub offset_addr_by_name_table: Option<u16>,
+    pub offset_addr_by_background_table: u16,
+    pub offset_addr_by_spri
