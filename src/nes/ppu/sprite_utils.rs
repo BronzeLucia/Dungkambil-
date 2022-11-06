8fd190e9f73e95218d5f8d3b0424e29fb3f22cc2
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-11-06 17:30:52 +0000

    500

--- src/nes/ppu/sprite_utils.rs
+++ src/nes/ppu/sprite_utils.rs
@@ -10,4 +10,10 @@ pub type SpritePosition = (u8, u8);
 pub struct SpriteConfig {
     pub offset_addr_by_name_table: Option<u16>,
     pub offset_addr_by_background_table: u16,
-    pub offset_addr_by_spri
+    pub offset_addr_by_sprite_table: u16,
+    pub is_horizontal_mirror: bool,
+    pub is_background_enable: bool,
+}
+
+pub fn mirror_down_sprite_addr(addr: Addr, is_horizontal_mirror: bool) -> Addr {
+    if !is_h
