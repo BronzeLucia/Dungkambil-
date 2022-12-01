f429b472825389337b1b92b2dd48bbeb061ed389
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-12-01 21:49:00 +0000

    registration

--- src/nes/ppu/tile.rs
+++ src/nes/ppu/tile.rs
@@ -1,2 +1,13 @@
 use self::super::super::Mmc;
-use se
+use self::super::palette::*;
+use self::super::sprite_utils::*;
+use self::super::Ram;
+
+#[derive(Debug)]
+pub struct Tile {
+    pub sprite: Sprite,
+    pub palette: PaletteList,
+}
+
+impl Tile {
+    pub f
