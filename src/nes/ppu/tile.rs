5c3a5a1903b0cda282015afcfe66b81ee9d50e71
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-12-03 22:55:08 +0000

    Removed old scripts

--- src/nes/ppu/tile.rs
+++ src/nes/ppu/tile.rs
@@ -21,4 +21,9 @@ impl Tile {
         // INFO see. http://hp.vector.co.jp/authors/VA042397/nes/ppu.html
         let block_id = get_block_id(position);
         let sprite_id = get_sprite_id(&vram, position, config);
-       
+        let attr = get_attribute(&vram, position, config);
+        let palette_id = (attr >> (block_id * 2)) & 0x03;
+        let sprite = build(
+            &cram,
+            sprite_id,
+            config.offset_addr_by_background
