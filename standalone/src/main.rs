39ec316001d850df4516fa11b0cea9551b19de1f
BronzeLucia <104293509+BronzeLucia@users.noreply.github.com>
2022-12-17 14:31:44 +0000

    address code review

--- standalone/src/main.rs
+++ standalone/src/main.rs
@@ -13,4 +13,13 @@ use std::time::{Duration, SystemTime};
 use std::env;
 use std::fs;
 use rustynes::nes;
-use rustyn
+use rustynes::nes::Context;
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 224;
+
+const PAD_A: u8      = 0x01;
+const PAD_B: u8      = 0x02;
+const PAD_SELECT: u8 = 0x04;
+const PAD_START: u8  = 0x08;
+const PAD_U: u8
